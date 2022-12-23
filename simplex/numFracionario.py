7e4c1e5901d32b040633529e3b83241b3cb78b3f
Kayo Vinicius <kayovinicius227@gmail.com>
2022-12-23 16:26:06 -0300

    simplex

--- simplex/numFracionario.py
+++ simplex/numFracionario.py
@@ -1,101 +1,85 @@
 from fractions import Fraction
 
-#Classe que representa um numero fracionario, extendendo fractions.Fraction, e implentando a lógica o "M grande"
 class NumFracionario(Fraction):
     def __init__(self,n,m=Fraction(0)):
         self.fraction = Fraction(n)
         self.m = Fraction(m)
    
     def __repr__(self):
-        """repr(self)"""
+        #repr(self)
         return str(float(self.fraction)) if self.m == 0 else str(float(self.fraction)) + ' + (' + str(float(self.m)) + '*M)'
 
-
     def __str__(self):
-        """str(self)"""
         return str(float(self.fraction)) if self.m == 0 else str(float(self.fraction)) + ' + (' + str(float(self.m)) + '*M)'
         
     def __eq__(self, f):
-        """a == b"""
+        #a == b
         if type(f) is not type(self):
             f = NumFracionario(f)
-            
         return self.fraction.__eq__(f.fraction) and self.m.__eq__(f.m)
 
     def __add__(self, f):
-        """a + b"""
+        #a + b
         if type(f) is not type(self):
-            f = NumFracionario(f)
-            
+            f = NumFracionario(f) 
         return NumFracionario(self.fraction.__add__(f.fraction),self.m.__add__(f.m))
 
     def ___sub__(self, f):
-        """a - b"""
+        #a - b
         if type(f) is not type(self):
             f = NumFracionario(f)
-            
         return NumFracionario(self.fraction.__sub__(f.fraction),self.m.___sub__(f.m))
 
     def __mul__(self, f):
-        """a * b"""
+        #a * b
         if type(f) is not type(self):
             f = NumFracionario(f)
-        
         if f.m == 0:
             return NumFracionario(self.fraction.__mul__(f.fraction))
         else:
             return NumFracionario(self.fraction.__mul__(f.fraction),self.m.__mul__(f.m)) 
 
     def __div__(self, f):
-        """a / b"""
+        #a / b
         if type(f) is not type(self):
             f = NumFracionario(f)
-        
         if f.m == 0:
             return NumFracionario(self.fraction.__div__(f.fraction))
         else:
             return NumFracionario(self.fraction.__div__(f.fraction),self.m.__div__(f.m))
     
     def __lt__(self, f):
-        """a < b"""
+        #a < b
         if type(f) is not type(self):
-            f = NumFracionario(f)
-            
+            f = NumFracionario(f) 
         if self.m == f.m:
             return self.fraction.__lt__(f.fraction)
-        
         else:   
             return self.m.__lt__(f.m)
         
     def __gt__(self, f):
-        """a > b"""
+        #a > b
         if type(f) is not type(self):
-            f = NumFracionario(f)
-            
+            f = NumFracionario(f) 
         if self.m == f.m:
             return self.fraction.__gt__(f.fraction)
-        
         else:   
             return self.m.__gt__(f.m)
 
     def __le__(self, f):
-        """a <= b"""
+        #a <= b
         if type(f) is not type(self):
-            f = NumFracionario(f)
-            
+            f = NumFracionario(f) 
         if self.m == f.m:
-            return self.fraction.__le__(f.fraction)
-        
+            return self.fraction.__le__(f.fraction)    
         else:   
             return self.m.__le__(f.m)
 
     def __ge__(self, f):
-        """a >= b"""
+        #a >= b
         if type(f) is not type(self):
-            f = NumFracionario(f)
-            
+            f = NumFracionario(f)  
         if self.m == f.m:
             return self.fraction.__ge__(f.fraction)
-        
         else:   
             return self.m.__ge__(f.m)
--- simplex/simplex.py
+++ simplex/simplex.py
@@ -1,16 +1,10 @@
 from numpy import matrix
 from numFracionario import NumFracionario
 
-#Classe que repesenta e resolve um PPL, usando o metodo do "M grande", quando necessário.
 class Tabela(object):
     def __init__(self, funcao_objetivo, restricoes=None):
-
-        #preenche as linhas da tabela das restrições
         self.linha_restricao = []
-        
         quatidade_variaveis_extra = len(restricoes) + len([(c,t,tr) for (c,t,tr) in restricoes if t!='<='])
-        
-        #preenche a linha da função objetivo
         self.linha_funcao_objetivo = [1] + [c*(-1) for c in funcao_objetivo] + [0]*quatidade_variaveis_extra + [0] 
         
         for i,(coeficientes, tipo, termo) in enumerate(restricoes):
@@ -23,23 +17,20 @@ class Tabela(object):
                 coluna_extra[i] = 1         
                 self.linha_funcao_objetivo[1 + len(coeficientes) + i] = NumFracionario(0, NumFracionario(1))
                       
-            elif tipo=='>=':
+            elif tipo == '>=':
                 coluna_extra[i] = -1
-                coluna_extra[i+1] = 1              
+                coluna_extra[i + 1] = 1              
                 self.linha_funcao_objetivo[1 + len(coeficientes) + i + 1] = NumFracionario(0, NumFracionario(1))
             
-            self.linha_restricao.append(self._converteParaF([0] + coeficientes + coluna_extra + [termo]))
+            self.linha_restricao.append(self.converteFracionario([0] + coeficientes + coluna_extra + [termo]))
         
-    def _converteParaF(self,lista):
+    def converteFracionario(self,lista):
         return [NumFracionario(e) for e in lista]
        
-    def printTabela(self):      
+    def imprimirTabela(self):      
         tabela = [self.linha_funcao_objetivo] + self.linha_restricao    
         print('\n', matrix([[str(f) for f in l] for l in tabela]))
-         
-    '''
-    Encontra a coluna corresponte a variavel que entra da base e retorna seu indice 
-    '''   
+           
     def _encontraEntra(self):
         menor_coeficiente = min(self.linha_funcao_objetivo[1:-1])
         
@@ -47,10 +38,7 @@ class Tabela(object):
             return None
         else:        
             return self.linha_funcao_objetivo[0:-1].index(menor_coeficiente)
- 
-    '''
-    Encontra a linha corresponte a variavel que sai da base e retorna seu indice 
-    ''' 
+  
     def _encontraSai(self, colunaDoPivo):   
         termos = [r[-1] for r in self.linha_restricao]
         variavel_entra = [r[colunaDoPivo] for r in self.linha_restricao]
@@ -58,51 +46,34 @@ class Tabela(object):
         razoes = []
         for i,termo in enumerate(termos):
             if variavel_entra[i] == 0:
-                razoes.append(NumFracionario(1,1)) #Usando o M grande para garantir que essa razao é sempre maior e portando a variavel não escolhida
-                #razoes.append(99999999 * abs(max(termos)))
+                razoes.append(NumFracionario(1,1))
             else:
-                razoes.append(termo/variavel_entra[i])
+                razoes.append(termo / variavel_entra[i])
                 
-        menor_razao_positiva = min([r for r in razoes if r > 0])        
-
-        return razoes.index(menor_razao_positiva)
- 
-    '''
-    Faz o pivoteamento, tendo como elemento pivo o elemento pi,pj da matrix de restrições
-    ''' 
+        menor_positivo = min([r for r in razoes if r > 0])        
+        return razoes.index(menor_positivo)
+  
     def pivoteamento(self, pi, pj):
-        #elemento pivo
         p = self.linha_restricao[pi][pj]     
-        #divide a linha do pivo pelo elemento pivo       
         self.linha_restricao[pi] = [x/p for x in self.linha_restricao[pi]]     
-        
-        #para cada elemento da linha correspondente a F.O. multiplica cada elemento da linha pivo
         tempLinha = [self.linha_funcao_objetivo[pj]* x for x in self.linha_restricao[pi]]  
-        #subtrai cada elemendo da linha da F.O. pelo falor calculado acima
         self.linha_funcao_objetivo = [self.linha_funcao_objetivo[i] - tempLinha[i] for i in range(len(tempLinha))]
         
-        #para cara linha corresponte a restricao i repete o mesmo procedimento feito na linha fa F.O.
         for i,restricao in enumerate(self.linha_restricao):
             if i != pi: 
                 tempLinha = [restricao[pj]* x for x in self.linha_restricao[pi]]         
                 self.linha_restricao[i] = [restricao[i] - tempLinha[i] for i in range(len(tempLinha))]   
- 
-    '''
-    Testa se a F.O. atual indica que a solução otima foi encontrada
-    ''' 
-    def _solucaoOtimaEncontrada(self):
+  
+    def solucaoOtimaEncontrada(self):
         if min(self.linha_funcao_objetivo[1:-1]) >= 0: 
             return True
         else:
             return False
-    
-    '''
-    Executa o metodo simplex
-    ''' 
+     
     def executar(self):
-        self.printTabela()
+        self.imprimirTabela()
         
-        while not self._solucaoOtimaEncontrada():
+        while not self.solucaoOtimaEncontrada():
             c = self._encontraEntra()
             r = self._encontraSai(c)
             
@@ -110,21 +81,21 @@ class Tabela(object):
             
             print('\nColuna do pivo: %s\nLinha do pivo: %s'%(c+1,r))
             
-            self.printTabela()
+            self.imprimirTabela()
     
     @property
     def variaveisDentroBase(self):      
-        dentroDaBase = [] 
+        dentro_base = [] 
         for c in range(1,len(self.linha_funcao_objetivo)-1):
-            valoresColuna = [l[c] for l in self.linha_restricao]
+            valor_coluna = [l[c] for l in self.linha_restricao]
             
-            numDeZeros = len([z for z in valoresColuna if z==NumFracionario(0)])
-            numDeUms = len([u for u in valoresColuna if u==NumFracionario(1)])
+            numDeZeros = len([z for z in valor_coluna if z==NumFracionario(0)])
+            numDeUms = len([u for u in valor_coluna if u==NumFracionario(1)])
             
             if numDeUms == 1 and numDeZeros == len(self.linha_restricao) - 1 :
-                dentroDaBase.append(c)
+                dentro_base.append(c)
 
-        return dentroDaBase
+        return dentro_base
     
     @property
     def variaveisForaBase(self):
@@ -132,7 +103,7 @@ class Tabela(object):
         
     @property  
     def solucaoOtima(self):
-        if not self._solucaoOtimaEncontrada():
+        if not self.solucaoOtimaEncontrada():
             self.executar() 
         
         dentro = self.variaveisDentroBase
@@ -152,7 +123,7 @@ class Tabela(object):
         
     @property
     def valorOtimo(self):
-        if not self._solucaoOtimaEncontrada():
+        if not self.solucaoOtimaEncontrada():
             self.executar()
         
         return self.linha_funcao_objetivo[-1]
